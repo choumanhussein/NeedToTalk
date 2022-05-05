0d4e2619524e3434c37d651b7bf02112ac66cff9
choumanhussein <chouman.hussein7@gmail.com>
2022-05-05 10:43:28 +0000

    users.guest.admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 user = User.create(name: "admin", email: "admin@admin.com", password: "123456", password_confirmation: "123456", admin: true)
+
+user = User.create(name: "guest_a", email: "guest_a@example.com", password: "guest_a", password_confirmation: "guest_a")
+
+user = User.create(name: "guest_b", email: "guest_b@example.com", password: "guest_b", password_confirmation: "guest_b")
+
+user = User.create(name: "admin", email: "admin_guest@admin.com", password: "123456", password_confirmation: "123456", admin: true)
+
+user = User.create(name: "Patrick", email: "Patrick@Patrick.com", password: "123456", password_confirmation: "123456", admin: true)
